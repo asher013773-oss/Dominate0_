9ab74c4e82fd318bb1582d901b24b69d60ce9c9a
asher013773-oss <asher013773@gmail.com>
2026-08-27 17:44:54 +0100

    Fix import statements for GoogleFont and Font

--- novacut/app/src/main/java/com/example/novacut/ui/theme/Type.kt
+++ novacut/app/src/main/java/com/example/novacut/ui/theme/Type.kt
@@ -6,8 +6,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.font.GoogleFont
+import androidx.compose.ui.text.font.Font
 import com.example.novacut.R
 
 val provider = GoogleFont.Provider(
