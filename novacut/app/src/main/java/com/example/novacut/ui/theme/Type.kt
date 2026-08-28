6a323551daecb4f5d19c96e36a90844b495af677
asher013773-oss <asher013773@gmail.com>
2026-08-28 03:25:49 +0100

    Remove unused import for GoogleFont

--- novacut/app/src/main/java/com/example/novacut/ui/theme/Type.kt
+++ novacut/app/src/main/java/com/example/novacut/ui/theme/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.example.novacut.R
