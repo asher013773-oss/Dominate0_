24b30cae36ce663cc533f777091ba4d2bfdb7834
asher013773-oss <asher013773@gmail.com>
2026-09-14 13:31:53 +0100

    Add Time.kt component with Card and BoxWithConstraints

---
+++ novacut/app/src/main/java/com/example/novacut/ui/components/Time.kt
@@ -0,0 +1,14 @@
+Card (
+  shape = RoundedCornerShape(16.dp),
+  elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp,
+            pressedElevation = 2.dp
+        )
+    ) {
+        BoxWithConstraints(
+            modifier = Modifier.size(width = 250.dp, height = 150.dp)
+        ) {
+            val boundWidth = maxWidth
+            val boundHeight = maxHeight
+           }
+  
