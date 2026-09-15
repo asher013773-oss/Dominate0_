1feba690fbb616ee6bb481fa4be7d115ebd52f02
asher013773-oss <asher013773@gmail.com>
2026-09-15 11:58:57 +0100

    Add kotlinx-coroutines-android dependency

--- novacut/app/build.gradle.kts
+++ novacut/app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.ui:ui-text-google-fonts")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
  implementation("androidx.navigation:navigation-compose:2.8.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
