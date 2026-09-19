31102570e560f2e9db68def54775bde4482c12ef
asher013773-oss <asher013773@gmail.com>
2026-09-19 10:56:18 +0100

    Fix formatting in Regulate.kt

--- novacut/app/src/main/java/com/example/novacut/ui/components/Regulate.kt
+++ novacut/app/src/main/java/com/example/novacut/ui/components/Regulate.kt
@@ -99,8 +99,7 @@ fun AnimatedWord(
                 modifier = Modifier
                     .offset(x = offsets[i].value.dp)
                     .alpha(alphas[i].value)
-                )
-            }
+            )
         }
     }
 }
